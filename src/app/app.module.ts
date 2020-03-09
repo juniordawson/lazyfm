22ce0b041b246637297d0ea12417dd266279cf81
juniordawson <contact@stevendawson.co.uk>
2020-03-09 22:24:41 +0000

    start of search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MixListComponent } from './mix-list/mix-list.component';
 import { MixPageComponent } from './mix-page/mix-page.component';
+import { SearchPipe } from './search.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MixListComponent,
-    MixPageComponent
+    MixPageComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
